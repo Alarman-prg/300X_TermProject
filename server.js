e25c51fb618855702260365b1a74c6ce9744b9fe
Alarman-prg <jepgpersonal@gmail.com>
2024-04-11 14:52:45 -0400

    npm init

---
+++ server.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.get("/", function (req, res) {
+res.send("Hello, World from Express!");
+});
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
+})
